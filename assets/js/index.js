cdc128ce32e4787a3b1b430a2cfc37f1f233bf70
monkindey <monkindey@163.com>
2015-09-10 10:41:36 +0800

    add record stop webview

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,30 @@
 (function($) {
-	window.AudioContext = window.AudioContext || window.webkitAudioContext;
-	var audio_context = new AudioContext;
-	var input = audio_context.createMediaStreamSource();
-	var recoder = new Recorder(input);
-	console.log(recoder);
+	// window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	// var audio_context = new AudioContext;
+	// var input = audio_context.createMediaStreamSource();
+	// var recoder = new Recorder(input);
+	// console.log(recoder);
+	
+
+	var $record = $('#record');
+	var $stop = $('#stop');
+
+	// 播放
+	$record.on('click', function() {
+		try{
+			control.record(1);
+		}catch(e) {
+
+		}
+	});
+
+	// 停止
+	$stop.on('click', function() {
+		try{
+			control.record(0);
+		}catch(e) {
+
+		}
+	});
+
 })(Zepto)
