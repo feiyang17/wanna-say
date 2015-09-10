0ea7f603b7ec340c4c81676265018e3c5a078c7e
monkindey <monkindey@163.com>
2015-09-11 02:47:36 +0800

    产品添加提示

--- assets/js/generate.js
+++ assets/js/generate.js
@@ -2,13 +2,37 @@
 	// 加载完页面执行
 	Zepto(function($) {
 		var $shareBtn = $('#share');
+		var $playBtn = $('#play');
+
+		var voice = document.getElementById('voice');
+		voice.src = audioSrc;
+
+		// alert(audioSrc);
+
+		// console.log(audioSrc + '....' + voice);
 
 		$('.album').addClass('album-drift');
-		$('.box').addClass('box-drift');
+		$('.box').addClass('box-drift').on('webkitAnimationEnd', function() {
+			$('#recording').hide();
+			$('#maked').show();
+		});
+
+		// 播放
+		$playBtn.on('click', function() {
+			voice.play();
+			try {
+				control.record(3);
+			} catch (e) {
+				console.log('----------play----------');
+			}
+		});
+
+		// 重做
 		$('#redo').on('click', function() {
 			window.location.href = './index.html';
 		});
 
+		// 分享
 		$shareBtn.on('click', function() {
 			try {
 				control.record(4);
