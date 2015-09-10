75412600c3ae88570b4d177da52ee5c5a3c1eef4
monkindey <monkindey@163.com>
2015-09-10 15:20:22 +0800

    save stop button

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,26 +4,36 @@
 	// var input = audio_context.createMediaStreamSource();
 	// var recoder = new Recorder(input);
 	// console.log(recoder);
-	
+
 
 	var $record = $('#record');
 	var $stop = $('#stop');
+	var $save = $('#save');
 
-	// 播放
+	// 开始
 	$record.on('click', function() {
-		try{
+		try {
 			control.record(1);
-		}catch(e) {
-
+		} catch (e) {
+			console.log(1);
 		}
 	});
 
 	// 停止
 	$stop.on('click', function() {
-		try{
+		try {
 			control.record(0);
-		}catch(e) {
+		} catch (e) {
+			console.log(0);
+		}
+	});
 
+	// 保存
+	$save.on('click', function() {
+		try {
+			control.record(2);
+		} catch (e) {
+			console.log(2);
 		}
 	});
 
